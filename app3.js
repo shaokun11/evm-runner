918a41fdddffdd19bf7911eed9151d2b442ccb59
shaokun <605416017@qq.com>
2023-09-13 13:35:38 +0800

    增加直接从storage读取值

---
+++ app3.js
@@ -0,0 +1,34 @@
+// 直接从storage中读取
+
+let url = "https://eth-sepolia.public.blastapi.io";
+import Web3 from "web3";
+const web3 = new Web3(new Web3.providers.HttpProvider(url));
+
+let addr = "0xE4beD49Fe64Ca88b990F2C51328f09291F7F50eb";
+const slot = 0;
+const paddedAddress = web3.utils.leftPad(addr, 64);
+const paddedSlot = web3.utils.padLeft(slot, 64);
+let index = web3.utils.soliditySha3(paddedAddress, paddedSlot);
+//  mapping(address account => uint256) private _balances;
+let res = await web3.eth.getStorageAt("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", index);
+console.log(res);
+
+let paddedAcc = web3.utils.padLeft(addr, 64);
+let paddedSender = web3.utils.padLeft("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", 64);
+let indexAllowance = web3.utils.soliditySha3(paddedSender, web3.utils.soliditySha3(paddedAcc, 1));
+
+//  mapping(address account => mapping(address spender => uint256)) private _allowances;
+res = await web3.eth.getStorageAt("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", indexAllowance);
+console.log(res);
+
+//  uint256 private _totalSupply;
+res = await web3.eth.getStorageAt("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", 2);
+console.log(res);
+
+//  string private _name;
+res = await web3.eth.getStorageAt("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", 3);
+console.log(res);
+
+//  string private _symbol;
+res = await web3.eth.getStorageAt("0x228C9731e289937FBD1B5bE7897522f2ecEb7630", 4);
+console.log(res);
