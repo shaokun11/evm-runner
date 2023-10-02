cba8ddaa36e612f44bd81f8051721103872c1739
shaokun <605416017@qq.com>
2023-10-02 09:32:33 +0800

    移除log

--- app4.js
+++ app4.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 let coder = ethers.AbiCoder.defaultAbiCoder();
 let code = coder.encode(["string", "string"], ["USDC", "USDC"]).slice(2);
-console.log("--------code---", code);
+// console.log("--------code---", code);
 
 console.log(coder.decode(["uint"], "0x00000000000000000000000000000000000000000000001043561a8829300000"));
 
@@ -154,14 +154,14 @@ let erc20_abi = [" function balanceOf(address owner) external view returns (uint
 let multi = new ethers.Interface(multi_abi);
 let erc20 = new ethers.Interface(erc20_abi);
 
-let call1_data = erc20.encodeFunctionData("balanceOf", ["0xE4dbFd60e3B20E2018dC07fd88148C0D7D966aB2"]);
-let call1_target = "0xE4dbFd60e3B20E2018dC07fd88148C0D7D966aB2";
 console.log(
     multi.encodeFunctionData("aggregate", [
         [
             {
-                callData: call1_data,
-                target: call1_target,
+                callData: erc20.encodeFunctionData("balanceOf", [
+                    "0xE4dbFd60e3B20E2018dC07fd88148C0D7D966aB2",
+                ]),
+                target: "0xeD9326aE94Bd18F1650d381FBe508a1e9b218271",
             },
         ],
     ])
