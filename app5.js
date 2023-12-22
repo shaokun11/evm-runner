61aeaa8c62d7e910947cd9a1c1f33dce8d6085db
shaokun <605416017@qq.com>
2023-12-22 11:56:47 +0800

    save

--- app5.js
+++ app5.js
@@ -1,14 +1,6 @@
-import Web3 from 'web3'
-// let a = Buffer.from("hello world", "utf8").toString("hex");
-// console.log(a);
-// console.log(Buffer.from("hello world", "utf8"));
-
-// let b = Buffer.from("68656c6c6f20776f726c64", "hex").toString();
-// console.log(b);
-
-const web3 = new Web3("https://rpc.ankr.com/eth_goerli	")
-// console.log(web3.eth.accounts.create())
-
-web3.eth.getTransactionReceipt("0x03fccf725e0b5802a1121959ddc237ed76549fe6129a9a9b78e7cb83a4c645ac").then(console.log)
-
-
+import Web3 from "web3";
+const web3 = new Web3(new Web3.providers.HttpProvider("https://mevm.devnet.m1.movementlabs.xyz/v1"));
+const res = await web3.eth.getTransactionReceipt(
+    "0x43465b887a3f4655f80b1b241ce08164f77a29ea704b6bcfd5004031c1982ff9"
+);
+console.log(res);
